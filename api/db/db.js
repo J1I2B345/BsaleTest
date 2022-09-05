f8575bf3c2933c2b98f1f4117fef0bfde8feb874
J1I2B345 <juan.bevilacqua@outlook.com>
2022-09-05 05:08:00 -0300

    node instead of nodemon

--- api/db/db.js
+++ api/db/db.js
@@ -2,19 +2,62 @@ const { Sequelize } = require("sequelize");
 const modelProduct = require("./models/product");
 const modelCategory = require("./models/category");
 
-const db = new Sequelize(
-	//DB name
-	process.env.DB_Name,
-	//Username
-	process.env.DB_User,
-	//password
-	process.env.DB_Password,
-	{
-		//host
-		host: process.env.DB_Host,
-		dialect: "mysql",
-	}
-);
+const db =
+	process.env.NODE_ENV === "production"
+		? new Sequelize(
+				//DB_Name
+				process.env.DB_Name,
+				//Username
+				process.env.DB_User,
+				//password
+				process.env.DB_Password,
+				{
+					//host
+					host: process.env.DB_Host,
+					dialect: "mysql",
+					port: 5432,
+					pool: {
+						max: 3,
+						min: 1,
+						idle: 10000,
+					},
+					dialectOptions: {
+						ssl: {
+							require: true,
+							// Ref.: https://github.com/brianc/node-postgres/issues/2009
+							rejectUnauthorized: false,
+						},
+						keepAlive: true,
+					},
+					ssl: true,
+				}
+		  )
+		: new Sequelize(
+				//DB name
+				process.env.DB_Name,
+				//Username
+				process.env.DB_User,
+				//password
+				process.env.DB_Password,
+				{
+					//host
+					host: process.env.DB_Host,
+					dialect: "mysql",
+					pool: {
+						max: 3,
+						min: 1,
+						idle: 10000,
+					},
+					dialectOptions: {
+						ssl: {
+							require: true,
+							// Ref.: https://github.com/brianc/node-postgres/issues/2009
+							rejectUnauthorized: false,
+						},
+					},
+					ssl: true,
+				}
+		  );
 
 modelProduct(db);
 modelCategory(db);
@@ -22,11 +65,6 @@ modelCategory(db);
 const { product, category } = db.models;
 product.belongsTo(category, { foreignKey: "category", as: "categoryId" });
 category.hasMany(product, { foreignKey: "category", as: "categoryId" });
-
-//keepAlive
-// setInterval(function () {
-// 	db.query("SELECT 1");
-// }, 4800);
 
 module.exports = {
 	...db.models,
